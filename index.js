c7415076023998545afe28322390407828aa9f88
user.email <jobayer.ahmed109@gmail.com>
2023-02-19 07:13:12 +0600

    final vercel

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.port || 5000;
 
 // middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
